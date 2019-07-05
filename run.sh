c8b9db0e68e66d158a5787be9bf89365be659768
Michael Schaller <michael.schaller@swednet.de>
2019-07-05 02:11:52 +0200

    Cleanup run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd /opt/TS3AudioBot
 
-echo "asdf" > /opt/TS3AudioBot/boink.txt
-
 if [ ! -f "/opt/TS3AudioBot/TS3AudioBot.exe" ]; then
     wget -O TS3AudioBot.zip https://splamy.de/api/nightly/ts3ab/master/download
     unzip TS3AudioBot.zip
