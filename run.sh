6da8c43b4388651b0f8728e87a796b3c080eec99
Sebastian Kunze <mail@sebastiankunze.de>
2019-07-05 08:49:50 +0200

    Remove .zip after unzip to save disk space

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ set -e
 if [ ! -f "TS3AudioBot.exe" ]; then
     wget -O TS3AudioBot.zip https://splamy.de/api/nightly/ts3ab/master/download
     unzip TS3AudioBot.zip
+    rm TS3AudioBot.zip
 fi
 
 mono TS3AudioBot.exe
